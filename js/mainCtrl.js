6497370feffa66515aa07c55ec59d1849319c2b7
Landon1013 <landon@thecallfamily.com>
2014-10-04 13:13:30 -0600

    Updates all pages

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,4 +2,18 @@ var app = angular.module('toDo');
 
 app.controller('mainCtrl', function($scope, mainService){
 
+    $scope.addItem = function(){
+        mainService.addItem($scope.addTodoItem);
+        $scope.addTodoItem = "";
+    };
+
+    $scope.removeItem = function(){
+        mainService.removeItem($scope.removeTodoItem);
+    }
+
+    var getItem = function(){
+        $scope.items = mainService.getItem();
+    }
+
+    getItem();
 });
--- js/mainService.js
+++ js/mainService.js
@@ -2,4 +2,25 @@ var app = angular.module('toDo');
 
 app.service('mainService', function(){
 
+    var items = [
+        "Pick up Dry Cleaning",
+        "Go to Gym"
+    ];
+
+    this.addItem = function(item){
+      items.push(item);
+    };
+
+    this.removeItem = function(item){
+        for(var i = 0; i < items.length; i++) {
+            if (items[i] === item) {
+                items.splice(i, 1);
+            }
+        }
+    };
+
+    this.getItem = function(){
+        return items;
+    };
+
 });
