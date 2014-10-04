8877cde1fd3d75aa4c0ad97f590c829b2ff90ab1
Landon1013 <landon@thecallfamily.com>
2014-10-04 12:15:21 -0600

    Links all pages together

---
+++ js/mainCtrl.js
@@ -0,0 +1,5 @@
+var app = angular.module('toDo');
+
+app.controller('mainCtrl', function($scope, mainService){
+
+});
---
+++ js/mainService.js
@@ -0,0 +1,5 @@
+var app = angular.module('toDo');
+
+app.service('mainService', function(){
+
+});
